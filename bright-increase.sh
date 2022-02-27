fa4226ca0bc61345823dfcb5c290e6967f052f0e
Vitor Guimarães <vitorguimaraes.ce@gmail.com>
2022-02-26 21:03:19 -0300

    Added: more color temperature values; Code reviewed

--- bright-increase.sh
+++ bright-increase.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
+
 # get screen name
 screen_name=$(xrandr | grep " connected" | cut -f1 -d " ")
 
-# get current brightness
 current_brightness=$(xrandr --verbose | awk '/Brightness/ {print $2}')
-
-# get current_gamma 
 current_gamma=$(xrandr --verbose | awk '/Gamma/ {print $2}')  
 
 # invert gamma
@@ -12,14 +11,14 @@ red=$(echo "$current_gamma" | cut -d':' -f1)
 green=$(echo "$current_gamma" | cut -d':' -f2)
 blue=$(echo "$current_gamma" | cut -d':' -f3)
 
-red=$(echo "scale=2; 1/$red" | bc)
-green=$(echo "scale=2; 1/$green" | bc)
-blue=$(echo "scale=2; 1/$blue" | bc)
+red=$(echo "scale=3; 1/$red" | bc)
+green=$(echo "scale=3; 1/$green" | bc)
+blue=$(echo "scale=3; 1/$blue" | bc)
 
 MAX_BRIGHTNESS=1.0
 
 # increase brightness
-current_brightness=$(echo "$current_brightness+0.1" | bc)
+current_brightness=$(echo "$current_brightness+0.02" | bc)
 if [ $(echo "$current_brightness <= $MAX_BRIGHTNESS" | bc) -eq 1 ]; then
     sed -i "s/default_brightness=.*/default_brightness=$current_brightness/" .zshrc
     xrandr --output $screen_name --brightness $current_brightness --gamma $red:$green:$blue
--- color_temp_decrease.sh
+++ color_temp_decrease.sh
@@ -1,35 +1,35 @@
+#!/bin/bash
+
+# Decrease
+# 2000 -> 6500
+declare -A temperatures
+temperatures[1.000:.555:.083]=1.00000000:0.64373109:0.28819679  
+temperatures[1.000:.625:.285]=1.00000000:0.71976951:0.42860152  
+temperatures[1.000:.714:.434]=1.00000000:0.77987699:0.54642268  
+temperatures[1.000:.769:.555]=1.00000000:0.82854786:0.64816570  
+temperatures[1.000:.833:.666]=1.00000000:0.86860704:0.73688797  
+temperatures[1.000:.833:.714]=1.00000000:0.90198230:0.81465502  
+temperatures[1.000:.909:.833]=1.00000000:0.93853986:0.88130458  
+temperatures[1.000:.909:.909]=1.00000000:0.97107439:0.94305985  
+temperatures[1.000:1.000:.909]=1.00000000:1.00000000:1.00000000 
+temperatures[1.000:1.000:1.000]=1.00000000:1.00000000:1.00000000 
+
 screen_name=$(xrandr | grep " connected" | cut -f1 -d " ")
 
-# get current brightness
 current_brightness=$(xrandr --verbose | awk '/Brightness/ {print $2}')  
-
-# get current_gamma 
 current_gamma=$(xrandr --verbose | awk '/Gamma/ {print $2}') 
 
-current_red=$(echo "$current_gamma" | cut -d':' -f1)
-current_green=$(echo "$current_gamma" | cut -d':' -f2)
-current_blue=$(echo "$current_gamma" | cut -d':' -f3)
-gamma=$(echo "$current_red"+"$current_green"+"$current_blue" | bc)
+red=$(echo "$current_gamma" | cut -d':' -f1)
+green=$(echo "$current_gamma" | cut -d':' -f2)
+blue=$(echo "$current_gamma" | cut -d':' -f3)
 
-if [ $(echo "$gamma >= 2.5 && $gamma < 2.6" | bc) -eq 1 ]; then 
-    target_gamma='0.80:0.80:1.0'
-elif [ $(echo "$gamma >= 2.6 && $gamma < 2.7" | bc) -eq 1 ]; then
-    target_gamma='0.80:0.90:1.0' 
-elif [ $(echo "$gamma >= 2.7 && $gamma < 2.8" | bc) -eq 1 ]; then
-    target_gamma='0.90:0.90:1.0'
-elif [ $(echo "$gamma >= 2.8 && $gamma <= 3" | bc) -eq 1 ]; then
-    target_gamma='1.0:1.0:1.0'
-fi
+red=$(echo "scale=3; 1/$red" | bc)
+green=$(echo "scale=3; 1/$green" | bc)
+blue=$(echo "scale=3; 1/$blue" | bc)
 
+gamma=$(echo ${temperatures[$red:$green:$blue]})
+echo $red:$green:$blue
+echo $gamma
 
-red=$(echo "$target_gamma" | cut -d':' -f1)
-green=$(echo "$target_gamma" | cut -d':' -f2)
-blue=$(echo "$target_gamma" | cut -d':' -f3)
-
-red=$(echo "scale=2; 1/$red" | bc)
-green=$(echo "scale=2; 1/$green" | bc)
-blue=$(echo "scale=2; 1/$blue" | bc)
-
-sed -i "s/default_gamma=.*/default_gamma=$target_gamma/" .zshrc
-xrandr --output $screen_name --brightness $(echo $current_brightness) --gamma $red:$green:$blue 
-
+xrandr --output $screen_name --brightness $(echo $current_brightness) --gamma $(echo $gamma)
+sed -i "s/default_gamma=.*/default_gamma=$gamma/" .zshrc
